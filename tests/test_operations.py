9bdd8d9081127f83d49eff28787db2f97fe71ad0
root <root@DanKin.localdomain>
2025-09-26 23:59:36 -0400

    Add unit test for multiplication function

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -10,3 +10,7 @@ def test_add():
 def test_subtract():
     """Test subtraction of numbers."""
     assert operations.subtract(5, 3) == 2
+
+def test_multiply():
+    """Test multiplication of numbers."""
+    assert operations.multiply(2, 4) == 8
