f71ed283c331f44f0947ffb97dd47a3be426387d
root <root@DanKin.localdomain>
2025-09-26 23:59:08 -0400

    Add unit test for subtraction function

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -7,3 +7,6 @@ def test_add():
     assert operations.add(2, 3) == 5
     assert operations.add(-1, 1) == 0
 
+def test_subtract():
+    """Test subtraction of numbers."""
+    assert operations.subtract(5, 3) == 2
